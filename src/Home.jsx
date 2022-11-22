230f2f04130f59696ee7319782065da2e8406b5f
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-11-22 13:46:33 +0200

    styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,13 +6,13 @@ const Home = () =>{
     return <div className="img-container">
         <div className="img-wrap">
         <Link to="/animals">
-            <h2>Animals</h2>
+            <h2>Animals collection</h2>
         </Link>
         <img src={require("./img/Gerbil.JPG")} alt="gerbil" />
         </div>
         <div className="img-wrap">
-        <Link to="/animals">
-            <h2>Birds</h2>
+        <Link to="/birds">
+            <h2>Birds collection</h2>
         </Link>
         <img src={require("./img/AT-nature-siberian-jay-winter-bird-walk.webp")} alt="gerbil" />
         </div>
