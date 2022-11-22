aa48e8b94d53aa2e852c4f9bba0052abe04d80b5
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2022-11-22 13:37:25 +0200

    Links and nav

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 const Home = () =>{
-    return <div>
-        <h1>home</h1>
+    return <div className="img-container">
+        <div className="img-wrap">
+        <Link to="/animals">
+            <h2>Animals</h2>
+        </Link>
+        <img src={require("./img/Gerbil.JPG")} alt="gerbil" />
+        </div>
+        <div className="img-wrap">
+        <Link to="/animals">
+            <h2>Birds</h2>
+        </Link>
+        <img src={require("./img/AT-nature-siberian-jay-winter-bird-walk.webp")} alt="gerbil" />
+        </div>
     </div>
 }
 
